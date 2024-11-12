5741caf9c8e66e1efbf20ea8910dac42f1feddd7
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-11-12 15:40:39 +0100

    better code version, with Prof's help

--- pokemon.py
+++ pokemon.py
@@ -17,6 +17,33 @@
 
 import random
 
+attacchi = {
+    "fire": {
+        "water": 0.6,
+        "electric": 1,
+        "grass": 1.4,
+        "fire": 1
+    },
+    "electric": {
+        "water": 1.4,
+        "grass": 0.6,
+        "fire": 1,
+        "electric": 1
+    },
+    "grass": {
+        "fire": 0.6,
+        "water": 1.4,
+        "electric": 1,
+        "grass": 1
+    },
+    "water": {
+        "fire": 1.4,
+        "grass": 0.6,
+        "electric": 0.6,
+        "water": 1
+    }
+}
+
 class Pokemon:
     def __init__(self, name, of_type, health, power):
         self.name = name
@@ -25,37 +52,9 @@ class Pokemon:
         self.of_type = of_type
 
     def attack(self, pokemon):
-     damage = self.power #danno base
-     if self.of_type == "water" and pokemon.of_type == "fire":
-        damage  *= 1.4 #applica il 40% del danno in più
-        print(f"That luck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is 40% more!")
-     elif self.of_type == "water" and pokemon.of_type == "electric":
-        damage  *= 0.6 #applica solo il 60% del danno
-        print(f"That unluck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is less than 40%!")
-     elif self.of_type == "water" and pokemon.of_type == "grass":
-        damage  *= 0.6 
-        print(f"That unluck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is less than 40%!")
-     elif self.of_type == "fire" and pokemon.of_type == "grass":
-        damage  *= 1.4 
-        print(f"That luck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is 40% more!")
-     elif self.of_type == "fire" and pokemon.of_type == "water":
-        damage  *= 0.6 
-        print(f"That unluck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is less than 40%!")    
-     elif self.of_type == "grass" and pokemon.of_type == "water":
-        damage  *= 1.4 
-        print(f"That luck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is 40% more!")
-     elif self.of_type == "grass" and pokemon.of_type == "fire":
-        damage  *= 0.6 
-        print(f"That unluck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is less than 40%!")    
-     elif self.of_type == "electric" and pokemon.of_type == "water":
-        damage  *= 1.4 
-        print(f"That luck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is 40% more!")
-     elif self.of_type == "electric" and pokemon.of_type == "grass":
-        damage *= 0.6 
-        print(f"That unluck {self.name}, {pokemon.name} is {pokemon.of_type} type ! Your damage is less than 40%!")
-        
-     pokemon.health -= damage 
-     print(f"{self.name} attack {pokemon.name} doing to it a damage of {damage} health points")
+
+     pokemon.health -= self.power * attacchi[self.of_type][pokemon.of_type]
+     print(f"{self.name} attack {pokemon.name} doing to it a damage of {self.power} health points")
      print(f"Now the {pokemon.name} health is of {pokemon.health} points")
 
 
@@ -71,37 +70,35 @@ pokemon_dict = {
     "squirtle": Pokemon("Squirtle", "water", 200, 38)
 }
 
-defeat = False
-
-while not defeat:
-   choose = input(f"Hi dear champion! Choose one Pokemon and start the battle! Type its name:\n" 
+choose = input(f"Hi dear champion! Choose one Pokemon and start the battle! Type its name:\n" 
                + "\n".join(pokemon_dict.keys()) + "\n").lower()
 
-   if choose in pokemon_dict:
-      chosen_pokemon = pokemon_dict[choose]
-      print(f"Congratulation, nice move! You have chose {chosen_pokemon.name}")
-      computer_pokemon = random.choice(list(pokemon_dict.values()))
-      print(f"The computer chose {computer_pokemon.name}! Be carefull!")
-      
-      while chosen_pokemon.health > 0 and computer_pokemon.health > 0:
-        chosen_pokemon.attack(computer_pokemon)
+while choose not in pokemon_dict:
+        print("Invalid Pokémon name. Please choose again.")
 
-        if computer_pokemon.health <= 0:
+chosen_pokemon = pokemon_dict[choose]
+print(f"Congratulation, nice move! You have chose {chosen_pokemon.name}")
+computer_pokemon = random.choice(list(pokemon_dict.values()))
+print(f"The computer chose {computer_pokemon.name}! Be carefull!")
+      
+while chosen_pokemon.health > 0 and computer_pokemon.health > 0:
+      chosen_pokemon.attack(computer_pokemon)
+
+      if computer_pokemon.health <= 0:
             print(f"{computer_pokemon.name} has been defeated! You have win!")
             defeat = True 
             break
 
-        elif chosen_pokemon.health <= 0:
+      computer_pokemon.attack(chosen_pokemon)
+
+      if chosen_pokemon.health <= 0:
             print(f"{chosen_pokemon.name} has been defeated! You have loose!")
             defeat = True
             break
 
-        next_or_quit = int(input((f"Do you have attack {computer_pokemon.name}! Did you want go next or quit? Press 1 or 2\n")))
-        if next_or_quit == 1:
-            computer_pokemon.attack(chosen_pokemon)
-        elif next_or_quit == 2:
-            print(f"Bye Bye!")
-            break
+      next_or_quit = int(input((f"Do you have attack {computer_pokemon.name}! Did you want go next or quit? Press 2 for quit or other for don't\n")))
+      if next_or_quit == 2:
+          print(f"Bye Bye!")
+          break
 
-   else:
-        print("Invalid Pokémon name. Please choose again.")
+
